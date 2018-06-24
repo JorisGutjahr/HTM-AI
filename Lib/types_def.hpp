38d8fee7ba92d1220b68f4018d0a384407a458c9
aiscratch <aiscratcher@gmx.de>
2018-06-24 19:36:18 +0200

    LICENSE for types_def.hpp

--- Lib/types_def.hpp
+++ Lib/types_def.hpp
@@ -1,5 +1,25 @@
+/*
+    htm-ai Implementation of the HTM Theory
+    Copyright (C) 2018 Joris Gutjahr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ */
 #ifndef __TYPES_DEFINED__
 #define __TYPES_DEFINED__
+
 namespace htm_ai {
   typedef unsigned int uint;
 }
---
+++ Lib/types_def.hpp~
@@ -0,0 +1,6 @@
+#ifndef __TYPES_DEFINED__
+#define __TYPES_DEFINED__
+namespace htm_ai {
+  typedef unsigned int uint;
+}
+#endif
